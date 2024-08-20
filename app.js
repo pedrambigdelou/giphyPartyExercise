e1c0b6d8a1cb770cca78cc8adb71fb64f0501824
pedrambigdelou <pedrambigdelou@gmail.com>
2024-08-20 11:03:34 -0400

    AJAX calls were created

--- app.js
+++ app.js
@@ -16,3 +16,20 @@ function addGif(res) {
     $gifArea.append($newCol);
   }
 }
+
+/* handle form submission: clear search box & make ajax call */
+
+$("form").on("submit", async function(evt) {
+  evt.preventDefault();
+
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+    }
+  });
+  addGif(response.data);
+});
