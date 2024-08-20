4966f06d875eb42208550887c93c6b1370c6f158
pedrambigdelou <pedrambigdelou@gmail.com>
2024-08-20 11:01:07 -0400

    the addGif function was created

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Let's get this party started!");
+const $gifArea = $("#gif-area");
+const $searchInput = $("#search");
+
+/* use ajax result to add a gif */
+
+function addGif(res) {
+  let numResults = res.data.length;
+  if (numResults) {
+    let randomIdx = Math.floor(Math.random() * numResults);
+    let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
+    let $newGif = $("<img>", {
+      src: res.data[randomIdx].images.original.url,
+      class: "w-100"
+    });
+    $newCol.append($newGif);
+    $gifArea.append($newCol);
+  }
+}
